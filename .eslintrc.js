93e8831a82e17cf33c737931c559bc89bb1d9133
Florian Diebold <florian.diebold@freiheit.com>
2018-10-30 11:42:32 +0100

    Fix lints

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": ["airbnb", "prettier"],
-    "env": {
-        "jest": true,
-        "browser": true
+    parser: "babel-eslint",
+    extends: ["airbnb", "prettier"],
+    env: {
+        jest: true,
+        browser: true
+    },
+    rules: {
+        "no-undef": 0, // checked by flow
+        "import/prefer-default-export": 0,
+        "arrow-body-style": 0,
+        "no-console": 0
     }
 };
